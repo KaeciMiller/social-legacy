a88c32e98ac4db1401c5e99315bf5b4c276d7719
Kaeci Mill <KevinGrand@kevin-grands-mac-pro-2.local>
2016-10-17 14:51:44 -0500

    Sign in UI custom classes complete

---
+++ Social Legacy 2/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Social Legacy 2
+//
+//  Created by Kaeci Mill on 10/17/16.
+//  Copyright © 2016 Clout, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Social Legacy 2/FancyField.swift
@@ -0,0 +1,21 @@
+//
+//  FancyField.swift
+//  Social Legacy 2
+//
+//  Created by Kaeci Mill on 10/17/16.
+//  Copyright © 2016 Clout, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
