9f44a8dda052427deee899d395c175facd14b255
Kaeci Mill <KevinGrand@kevin-grands-mac-pro-2.local>
2016-10-28 16:49:02 -0500

    Completed part 2 of feed ui

--- Social Legacy 2/CircleView.swift
+++ Social Legacy 2/CircleView.swift
@@ -15,7 +15,7 @@ class CircleView: UIImageView {
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 1.0
-        layer.shadowRadius = 6.0
+        layer.shadowRadius = 2.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         
     }
---
+++ Social Legacy 2/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Social Legacy 2
+//
+//  Created by Kaeci Mill on 10/28/16.
+//  Copyright © 2016 Clout, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      
+    }
+
+   
+    
+}
